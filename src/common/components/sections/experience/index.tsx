cc44f19cf6f2dbd2f944b4e391130a03bcf7aae1
ajfeng4 <ajfeng@buffalo.edu>
2025-01-05 19:41:48 -0500

    Fixed and added logo

--- src/common/components/sections/experience/index.tsx
+++ src/common/components/sections/experience/index.tsx
@@ -1,26 +1,42 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { useTheme } from 'next-themes';
-import { useHasMounted, useSectionInView } from '@/common/lib/hooks';
-import SectionHeading from '@/common/components/shared/section-heading';
-import { experiencesData } from '@/common/lib/data';
-import SectionDivider from '@/common/components/shared/section-divider';
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Image, { StaticImageData } from "next/image";
+import { useTheme } from "next-themes";
+import { useHasMounted, useSectionInView } from "@/common/lib/hooks";
+import SectionHeading from "@/common/components/shared/section-heading";
+import { experiencesData } from "@/common/lib/data";
+import SectionDivider from "@/common/components/shared/section-divider";
 
 export default function Experience() {
-  const { ref } = useSectionInView('experience');
+  const { ref } = useSectionInView("experience");
   const { theme } = useTheme();
   const hasMounted = useHasMounted();
-  const [selectedSection, setSelectedSection] = useState('experience');
+  const [selectedSection, setSelectedSection] = useState("experience");
 
   const filteredData = experiencesData.filter(
       (item) => item.section === selectedSection
   );
+
+  function getIconElement(icon: React.ReactNode | StaticImageData, title: string) {
+    if (icon && typeof icon === "object" && "src" in icon) {
+      return (
+          <Image
+              src={icon as StaticImageData}
+              alt={title}
+              width={50}
+              height={50}
+              style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
+      );
+    }
+    return icon;
+  }
 
   return (
       <section
@@ -31,31 +47,31 @@ export default function Experience() {
         <SectionHeading>My experience</SectionHeading>
         <div className="flex justify-center space-x-4 mb-6">
           <button
-              onClick={() => setSelectedSection('experience')}
+              onClick={() => setSelectedSection("experience")}
               className={`px-4 py-2 rounded-md text-sm font-medium ${
-                  selectedSection === 'experience'
-                      ? 'bg-blue-600 text-white'
-                      : 'bg-gray-200 dark:bg-gray-700 text-black dark:text-white'
+                  selectedSection === "experience"
+                      ? "bg-blue-600 text-white"
+                      : "bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
               }`}
           >
             Experiences
           </button>
           <button
-              onClick={() => setSelectedSection('education')}
+              onClick={() => setSelectedSection("education")}
               className={`px-4 py-2 rounded-md text-sm font-medium ${
-                  selectedSection === 'education'
-                      ? 'bg-blue-600 text-white'
-                      : 'bg-gray-200 dark:bg-gray-700 text-black dark:text-white'
+                  selectedSection === "education"
+                      ? "bg-blue-600 text-white"
+                      : "bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
               }`}
           >
             Education
           </button>
           <button
-              onClick={() => setSelectedSection('extracurriculars')}
+              onClick={() => setSelectedSection("extracurriculars")}
               className={`px-4 py-2 rounded-md text-sm font-medium ${
-                  selectedSection === 'extracurriculars'
-                      ? 'bg-blue-600 text-white'
-                      : 'bg-gray-200 dark:bg-gray-700 text-black dark:text-white'
+                  selectedSection === "extracurriculars"
+                      ? "bg-blue-600 text-white"
+                      : "bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
               }`}
           >
             Extracurriculars
@@ -64,40 +80,48 @@ export default function Experience() {
         <VerticalTimeline animate={false}>
           {hasMounted &&
               filteredData.map((item, index) => (
-                  <React.Fragment key={index}>
-                    <VerticalTimelineElement
-                        contentStyle={{
-                          background:
-                              theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
-                          boxShadow: 'none',
-                          border: '1px solid rgba(0, 0, 0, 0.05)',
-                          textAlign: 'left',
-                          padding: '1.3rem 2rem',
-                        }}
-                        contentArrowStyle={{
-                          borderRight:
-                              theme === 'light'
-                                  ? '0.4rem solid #9ca3af'
-                                  : '0.4rem solid rgba(255, 255, 255, 0.5)',
-                          display: theme === 'dark' ? 'none' : 'block',
-                        }}
-                        date={item.date}
-                        icon={item.icon}
-                        iconStyle={{
-                          background:
-                              theme === 'light' ? '#fff' : 'rgba(255, 255, 255, 0.15)',
-                          fontSize: '1.5rem',
-                          display: theme === 'dark' ? 'none' : 'flex',
-                        }}
-                    >
-                      <h3 className="font-semibold capitalize">{item.title}</h3>
-                      <p className="!mt-0 !mb-1 font-medium">{item.company}</p>
-                      <p className="!mt-0 font-normal">{item.location}</p>
-                      <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                        {item.description}
-                      </p>
-                    </VerticalTimelineElement>
-                  </React.Fragment>
+                  <VerticalTimelineElement
+                      key={index}
+                      contentStyle={{
+                        background:
+                            theme === "light"
+                                ? "#f3f4f6"
+                                : "rgba(255, 255, 255, 0.05)",
+                        boxShadow: "none",
+                        border: "1px solid rgba(0, 0, 0, 0.05)",
+                        textAlign: "left",
+                        padding: "1.3rem 2rem",
+                      }}
+                      contentArrowStyle={{
+                        borderRight:
+                            theme === "light"
+                                ? "0.4rem solid #9ca3af"
+                                : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                        display: theme === "dark" ? "none" : "block",
+                      }}
+                      date={item.date}
+                      icon={getIconElement(item.icon, item.title)}
+                      iconStyle={{
+                        background:
+                            theme === "light"
+                                ? "#fff"
+                                : "rgba(255, 255, 255, 0.15)",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderRadius: "50%",
+                        overflow: "hidden",
+                        width: "50px",
+                        height: "50px",
+                      }}
+                  >
+                    <h3 className="font-semibold capitalize">{item.title}</h3>
+                    <p className="!mt-0 !mb-1 font-medium">{item.company}</p>
+                    <p className="!mt-0 font-normal">{item.location}</p>
+                    <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                      {item.description}
+                    </p>
+                  </VerticalTimelineElement>
               ))}
         </VerticalTimeline>
         <div className="flex w-full justify-center dark:bg-darkBg">
